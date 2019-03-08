385dd7d824f71d5ce2b35b240ad13fed214717df
jasoncocks <40471681+jasoncocks@users.noreply.github.com>
2019-03-08 10:53:46 +0000

    Add files via upload

---
+++ BadMailSend.ps1
@@ -0,0 +1,28 @@
+﻿$INETPUBHome = "C:\inetpub\mailroot"
+$BadMail = "$INETPUBHome\BadMail"
+$Pickup = "$INETPUBHome\Pickup"
+
+stop-service -Name SMTPSVC
+
+# create BadMail backup
+$BadMail_Backup = "$INETPUBHome\BadMail_Backup_$(get-date -f yyyyMMddHHmmss)"
+
+Write-Host "Create BadMail backup to: $BadMail_Backup"
+Copy-Item -Recurse -Path $BadMail -Destination $BadMail_Backup
+
+Write-Host "Cleaning BadMail ..."
+Remove-Item –path $BadMail\* -Exclude *.bad
+
+Write-Host "Processing *.bad from: $BadMail"
+
+foreach ($f in Get-ChildItem -Path $BadMail -Filter *.bad) {
+    $smpt_body = Get-Content -Path $f.FullName -Raw
+
+    $r = $smpt_body -replace "(?smi)From:[^!]+From:", "From:"
+
+    $r | Out-File -FilePath $Pickup\$($f.BaseName) -Encoding ascii
+
+    Remove-Item $f.FullName
+}
+
+start-service -Name SMTPSVC
